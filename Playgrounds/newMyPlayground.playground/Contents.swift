4f4aa243549fbba175178d29222498d08754685f
Jasbeer Singh <38327762+jass1313@users.noreply.github.com>
2018-09-15 18:17:47 +0530

    nnn

--- Playgrounds/newMyPlayground.playground/Contents.swift
+++ Playgrounds/newMyPlayground.playground/Contents.swift
@@ -1,3 +1,56 @@
 import UIKit
 
 var str = "Hello, playground"
+
+let name : String = "Jasbeer "
+
+var age : Int = 26
+
+age = 36
+
+let myFullName = name + " Singh"
+
+let myDetails = "\(myFullName), \(age)"
+let myDetail = myFullName + String(age)
+
+let boolean : Bool = true
+
+var double : Double = 12.4849392
+
+let float : Float = 3.50
+
+double = 40.0000
+
+//Bank account
+let bankName : String =  "Icici Bank"
+let ifscCode : String = "Icic000256"
+let accountHolderName : String = "Jasbeer Singh"
+let accountNumber : Int = 073101000196
+var bankBalance : Float = 1000.80
+var depositAmount : Int = 2000
+var newBankBalance = Int(bankBalance) + depositAmount
+
+print(newBankBalance)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
