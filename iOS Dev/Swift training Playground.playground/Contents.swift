9de3eb93cb43bbe60096064af54c02f7a3729957
Jasbeer Singh <jass0013@hotmail.com>
2019-02-04 18:50:12 +0530

    cv

--- iOS Dev/Swift training Playground.playground/Contents.swift
+++ iOS Dev/Swift training Playground.playground/Contents.swift
@@ -171,3 +171,211 @@ print("The status message is \(http200Status.description)")
 // Prints "The status message is OK"
 
 //Optionals
+let possibleNumber = "123"
+let convertedNumber = Int(possibleNumber)
+// convertedNumber is inferred to be of type "Int?", or "optional Int"
+
+//nil
+var serverResponseCode: Int? = 404
+// serverResponseCode contains an actual Int value of 404
+serverResponseCode = nil
+// serverResponseCode now contains no value
+
+var surveyAnswer: String?
+// surveyAnswer is automatically set to nil
+
+//If Statements and Forced Unwrapping
+if convertedNumber != nil {
+    print("convertedNumber contains some integer value.")
+}
+// Prints "convertedNumber contains some integer value."
+
+if convertedNumber != nil {
+    print("convertedNumber has an integer value of \(convertedNumber!).")
+}
+// Prints "convertedNumber has an integer value of 123."
+
+
+//Optional Binding
+if let actualNumber = Int(possibleNumber) {
+    print("The string \"\(possibleNumber)\" has an integer value of \(actualNumber)")
+} else {
+    print("The string \"\(possibleNumber)\" could not be converted to an integer")
+}
+// Prints "The string "123" has an integer value of 123"
+
+if let firstNumber = Int("10"), let secondNumber = Int("42"),
+    firstNumber < secondNumber && secondNumber < 100 {
+    print("\(firstNumber) < \(secondNumber) < 100")
+}
+// Prints "4 < 42 < 100"
+
+if let firstNumber = Int("40") {
+    if let secondNumber = Int("42") {
+        if firstNumber < secondNumber && secondNumber < 100 {
+            print("\(firstNumber) < \(secondNumber) < 100")
+        }
+    }
+}
+// Prints "4 < 42 < 100"
+
+//Implicitly Unwrapped Optionals
+let possibleString: String? = "An optional string."
+let forcedString: String = possibleString! // requires an exclamation mark
+
+let assumedString: String! = "An implicitly unwrapped optional string."
+let implicitString: String = assumedString // no need for an exclamation mark its automatically unwrapped
+
+if assumedString != nil {
+    print(assumedString!)
+}
+// Prints "An implicitly unwrapped optional string."
+
+if let definiteString = assumedString {
+    print(definiteString)
+}
+// Prints "An implicitly unwrapped optional string."
+
+
+//Error Handling
+func canThrowAnError() throws {
+    // this function may or may not throw an error
+}
+do {
+    try canThrowAnError()
+    // no error was thrown
+} catch {
+    // an error was thrown
+}
+
+func makeASandwich() throws {
+    // ...
+}
+
+/*do {
+    try makeASandwich()
+    eatASandwich()
+} catch SandwichError.outOfCleanDishes {
+    washDishes()
+} catch SandwichError.missingIngredients(let ingredients) {
+    buyGroceries(ingredients)
+}*/
+
+
+
+
+//Assertions and Preconditions
+//Debugging with Assertions
+let age = 3
+assert(age >= 0, "A person's age can't be less than zero.")
+// This assertion fails because -3 is not >= 0.
+
+if age > 10 {
+    print("You can ride the roller-coaster or the ferris wheel.")
+} else if age >= 0 {
+    print("You can ride the ferris wheel.")
+} else {
+    assertionFailure("A person's age can't be less than zero.")
+}
+
+
+//Enforcing Preconditions
+var index = 1
+// In the implementation of a subscript...
+precondition(index > 0, "Index must be greater than zero.")
+
+if index > 10 {
+    print("You can ride the roller-coaster or the ferris wheel.")
+} else if index >= 0 {
+    print("You can ride the ferris wheel.")
+} else {
+    preconditionFailure("A person's age can't be less than zero.")
+   // fatalError("Unimplemented")
+}
+
+/*
+//Basic Operators
+Assignment Operator
+let b = 10
+var a = 5
+a = b
+ a is now equal to 10
+let (x, y) = (1, 2)
+ x is equal to 1, and y is equal to 2
+if x = y {
+    This is not valid, because x = y does not return a value.
+*/
+
+
+/*
+//Arithmetic Operators
+1 + 2       // equals 3
+5 - 3       // equals 2
+2 * 3       // equals 6
+10.0 / 2.5  // equals 4.0
+hello, " + "world"
+ */
+
+/*
+ //Remainder Operator
+9 % 4    // equals 1
+-9 % 4   // equals -1
+-9 = (4 x -2) + -1
+//giving a remainder value of -1.
+*/
+
+//Unary Minus Operator
+let three1 = 3
+let minusThree = -three1      // minusThree equals -3
+let plusThree = -minusThree   // plusThree equals 3, or "minus minus three"
+
+
+//Unary Plus Operator
+let minusSix = +6
+let alsoMinusSix = -minusSix  // alsoMinusSix equals -6
+
+
+//Compound Assignment Operators
+var a = 1
+a += 2
+// a is now equal to 3
+//no return value avliable
+
+
+//Comparison Operators
+/*
+ Equal to (a == b)
+Not equal to (a != b)
+Greater than (a > b)
+Less than (a < b)
+Greater than or equal to (a >= b)
+Less than or equal to (a <= b)
+*/
+// two identity operators (=== and !==)
+
+1 == 1   // true because 1 is equal to 1
+2 != 1   // true because 2 is not equal to 1
+2 > 1    // true because 2 is greater than 1
+1 < 2    // true because 1 is less than 2
+1 >= 1   // true because 1 is greater than or equal to 1
+2 <= 1   // false because 2 is not less than or equal to 1
+
+let name = "world"
+if name == "world" {
+    print("hello, world")
+} else {
+    print("I'm sorry \(name), but I don't recognize you")
+}
+// Prints "hello, world", because name is indeed equal to "world".
+
+
+//Comparison Operators with tuples
+(1, "zebra") < (2, "apple")   // true because 1 is less than 2; "zebra" and "apple" are not compared
+(3, "apple") <= (3, "bird")    // true because 3 is equal to 3, and "apple" is less than "bird"
+(4, "dog") == (4, "dog")      // true because 4 is equal to 4, and "dog" is equal to "dog"
+
+("blue", -1) < ("purple", 1)        // OK, evaluates to true
+//("blue", false) < ("purple", true)  // Error because < can't compare Boolean values
+
+
+//Ternary Conditional Operator
