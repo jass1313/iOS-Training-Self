161c109a739dccbbf57c80e67d03d0c6b798e740
Jasbeer Singh <jass0013@hotmail.com>
2019-02-06 18:25:17 +0530

    njj

---
+++ CleanSwiftDemo/CleanSwiftDemo/LoginApiStore.swift
@@ -0,0 +1,16 @@
+//
+//  LoginApiStore.swift
+//  CleanSwiftDemo
+//
+//  Created by KiwiTech on 28/01/19.
+//  Copyright © 2019 KiwiTech. All rights reserved.
+//
+
+import UIKit
+
+class LoginApiStore: NSObject {
+
+    func loginAPIHitKaro(request: Login.Something.Request, completion: (Login.Something.Response) -> Void) {
+        //
+    }
+}
