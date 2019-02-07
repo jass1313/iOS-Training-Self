eaa0e1ff4b10dd29169a7fff5f68c1c4e8f90b0e
Jasbeer Singh <jass0013@hotmail.com>
2019-02-07 18:24:15 +0530

    hi

--- iOS Dev/Swift training Playground.playground/Contents.swift
+++ iOS Dev/Swift training Playground.playground/Contents.swift
@@ -455,7 +455,7 @@ for name in names[..<2] {
 // Anna
 // Alex
 
-let range = 1...5
+let range = ...5
 range.contains(100)   // false
 range.contains(4)   // true
 range.contains(-1)  // true
@@ -513,3 +513,333 @@ if (enteredDoorCode && passedRetinaScan) || hasDoorKey || knowsOverridePassword 
 
 
 //3.Strings and Characters
+//String Literals
+let someString = "Some string literal value"
+
+//Multiline String Literals
+let quotation = """
+The White Rabbit put on his spectacles.  "Where shall I begin,
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on
+till you come to the end; then stop."
+"""
+let singleLineString = "These are the same."
+let multilineString = """
+These are the same.
+"""
+
+let softWrappedQuotation = """
+The White Rabbit put on his spectacles.  "Where shall I begin, \
+please your Majesty?" he asked.
+
+"Begin at the beginning," the King said gravely, "and go on \
+till you come to the end; then stop."
+"""
+print(softWrappedQuotation)
+
+let lineBreaks = """
+
+This string starts with a line break.
+It also ends with a line break.
+
+"""
+print(lineBreaks)
+
+let lineWithIndenttation = """
+   This string starts with a line break.    
+It also ends with a line break.
+"""
+print(lineWithIndenttation)
+
+
+//Special Characters in String Literals
+let wiseWords = "\"Imagination is more important than knowledge\" - Einstein"
+// "Imagination is more important than knowledge" - Einstein
+let dollarSign = "\u{0024}"        // $,  Unicode scalar U+0024
+let blackHeart = "\u{2665}"      // ♥,  Unicode scalar U+2665
+let sparklingHeart = "\u{1F496}" // 💖, Unicode scalar U+1F496
+
+
+let threeDoubleQuotationMarks = """
+Escaping the first quotation mark \"""
+Escaping all three quotation marks \r\"\"
+"""
+print(threeDoubleQuotationMarks)
+
+//Initializing an Empty String
+var emptyString = ""               // empty string literal
+var anotherEmptyString = String()  // initializer syntax
+// these two strings are both empty, and are equivalent to each other
+
+if emptyString.isEmpty {
+    print("Nothing to see here")
+}
+// Prints "Nothing to see here"
+
+//String Mutability
+var variableString = "Horse"
+variableString += " and carriage"
+// variableString is now "Horse and carriage"
+
+/*let constantString = "Highlander"
+constantString += " and another Highlander"
+// this reports a compile-time error - a constant string cannot be modified
+*/
+//Strings Are Value Types
+//??
+
+//Working with Characters
+for character in "Dog!🐶" {
+    print(character)
+}
+// D
+// o
+// g
+// !
+// 🐶
+
+let exclamationMark: Character = "!"
+
+let catCharacters: [Character] = ["C", "a", "t", "!", "🐱"]
+print(catCharacters)
+let catString = String(catCharacters)
+print(catString)
+
+for char in catCharacters[0...] {
+    print(char)
+}
+for i in 0..<5 {
+    print(catCharacters[i])
+}
+// Prints "Cat!🐱"
+
+//Random number assign to varible in every run value will change
+var range1 = Int.random(in: 1...100)
+print(range1)
+
+
+//Concatenating Strings and Characters
+let string1 = "hello"
+let string2 = " there"
+var welcome = string1 + string2
+welcome += " how are you"
+
+// welcome now equals "hello there"
+var instruction = "look over"
+instruction += string2
+// instruction now equals "look over there"
+
+//add characher in string with using append
+welcome.append(exclamationMark)
+// welcome now equals "hello there!"
+
+
+let badStart = """
+one
+two
+"""
+let end = """
+three
+"""
+print(badStart + end)
+// Prints two lines:
+// one
+// twothree
+
+let goodStart = """
+one
+two
+
+"""
+print(goodStart + end)
+// Prints three lines:
+// one
+// two
+// three
+
+
+//String Interpolation
+let multiplier = 3
+let message = "\(multiplier) times 2.5 is \(Double(multiplier) * 2.5)"
+// message is "3 times 2.5 is 7.5"
+
+//Unicode
+let eAcute: Character = "\u{E9}"                         // é
+let combinedEAcute: Character = "\u{65}\u{301}"          // e followed by
+// eAcute is é, combinedEAcute is é
+
+let precomposed: Character = "\u{D55C}"                  // 한
+let decomposed: Character = "\u{1112}\u{1161}\u{11AB}"   // ᄒ, ᅡ, ᆫ
+// precomposed is 한, decomposed is 한
+
+let enclosedEAcute: Character = "\u{E9}\u{20DD}"
+// enclosedEAcute is é⃝
+
+let regionalIndicatorForUS: Character = "\u{1F1FA}\u{1F1F8}"
+// regionalIndicatorForUS is 🇺🇸
+
+//Counting Characters
+let unusualMenagerie = "Koala 🐨, Snail 🐌, Penguin 🐧, Dromedary 🐪"
+print("unusualMenagerie has \(unusualMenagerie.count) characters")
+// Prints "unusualMenagerie has 40 characters"
+
+var word = "cafe"
+print("the number of characters in \(word) is \(word.count)")
+// Prints "the number of characters in cafe is 4"
+
+word += "\u{301}"    // COMBINING ACUTE ACCENT, U+0301
+
+print("the number of characters in \(word) is \(word.count)")
+// Prints "the number of characters in café is 4"
+
+
+
+//Accessing and Modifying a String[D]
+let greeting = "Guten Tag!"
+greeting[greeting.startIndex]
+// G
+greeting[greeting.index(before: greeting.endIndex)]
+// !
+greeting[greeting.index(after: greeting.startIndex)]
+// u
+
+for i in 0..<10 {
+let index1 = greeting.index(greeting.startIndex, offsetBy: i)
+print(greeting[index1])
+}
+
+for index in greeting.indices {
+    print("\(greeting[index])", terminator: "")
+}
+// Prints "G u t e n   T a g ! "
+
+//Inserting and Removing from string
+//Inserting
+var welcome1 = "hello"
+welcome1.insert("!", at: welcome1.endIndex)
+// welcome now equals "hello!"
+welcome1.insert(contentsOf: " there", at: welcome1.index(before: welcome1.endIndex))
+// welcome now equals "hello there!"
+
+//Removing
+welcome1.remove(at: welcome1.index(before: welcome1.endIndex))
+// welcome now equals "hello there"
+let range2 = welcome1.index(welcome1.endIndex, offsetBy: -6)..<welcome1.endIndex
+welcome1.removeSubrange(range2)
+// welcome now equals "hello"
+
+
+
+//Substrings
+let greeting3 = "Hello, world!"
+let index3 = greeting3.firstIndex(of: ",") ?? greeting3.endIndex
+//substring its us the memory of string
+let beginning = greeting3[..<index3]
+// beginning is "Hello"
+// Convert the result to a String for long-term storage.
+let newString = String(beginning)
+
+
+
+//Comparing Strings
+//String and Character Equality
+let quotation1 = "We're a lot alike, you and I."
+let sameQuotation = "We're a lot alike, you and I."
+if quotation1 == sameQuotation {
+    print("These two strings are considered equal")
+}
+// Prints "These two strings are considered equal"
+
+
+// "Voulez-vous un café?" using LATIN SMALL LETTER E WITH ACUTE
+let eAcuteQuestion1 = "Voulez-vous un caf\u{E9}?"
+// "Voulez-vous un café?" using LATIN SMALL LETTER E and COMBINING ACUTE ACCENT
+let combinedEAcuteQuestion1 = "Voulez-vous un caf\u{65}\u{301}?"
+if eAcuteQuestion1 == combinedEAcuteQuestion1 {
+    print("These two strings are considered equal")
+}
+// Prints "These two strings are considered equal"
+
+
+//English A
+let latinCapitalLetterA: Character = "\u{41}"
+//Russian A
+let cyrillicCapitalLetterA: Character = "\u{0410}"
+//Thats why that are not same with each other
+if latinCapitalLetterA != cyrillicCapitalLetterA {
+    print("These two characters are not equivalent.")
+}
+// Prints "These two characters are not equivalent."
+
+//Prefix and Suffix Equality
+let romeoAndJuliet = [
+    "Act 1 Scene 1: Verona, A public place",
+    "Act 1 Scene 2: Capulet's mansion",
+    "Act 1 Scene 3: A room in Capulet's mansion",
+    "Act 1 Scene 4: A street outside Capulet's mansion",
+    "Act 1 Scene 5: The Great Hall in Capulet's mansion",
+    "Act 2 Scene 1: Outside Capulet's mansion",
+    "Act 2 Scene 2: Capulet's orchard",
+    "Act 2 Scene 3: Outside Friar Lawrence's cell",
+    "Act 2 Scene 4: A street in Verona",
+    "Act 2 Scene 5: Capulet's mansion",
+    "Act 2 Scene 6: Friar Lawrence's cell"
+]
+var act1SceneCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasPrefix("Act 1 ") {
+        act1SceneCount += 1
+    }
+}
+print("There are \(act1SceneCount) scenes in Act 1")
+// Prints "There are 5 scenes in Act 1"
+
+
+var mansionCount = 0
+var cellCount = 0
+for scene in romeoAndJuliet {
+    if scene.hasSuffix("Capulet's mansion") {
+        mansionCount += 1
+    } else if scene.hasSuffix("Friar Lawrence's cell") {
+        cellCount += 1
+    }
+}
+print("\(mansionCount) mansion scenes; \(cellCount) cell scenes")
+// Prints "6 mansion scenes; 2 cell scenes"
+
+
+
+
+//Unicode Representations of Strings
+let dogString = "Dog‼🐶"
+for codeUnit in dogString.utf8 {
+    print("\(codeUnit) ", terminator: "")
+}
+print("")
+// Prints "68 111 103 226 128 188 240 159 144 182 "
+
+for codeUnit in dogString.utf16 {
+    print("\(codeUnit) ", terminator: "")
+}
+print("")
+// Prints "68 111 103 8252 55357 56374 "
+
+for scalar in dogString.unicodeScalars {
+    print("\(scalar.value) ", terminator: "")
+}
+print("")
+// Prints "68 111 103 8252 128054 "
+
+for scalar in dogString.unicodeScalars {
+    print("\(scalar) ")
+}
+// D
+// o
+// g
+// ‼
+// 🐶
+
+
+//4.Collection Types (Arrys, Set, Dictionaries)
+//Arrays
