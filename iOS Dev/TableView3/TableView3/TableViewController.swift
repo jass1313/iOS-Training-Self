96ba3091165cc1b70bed01f7e7d59c52508ef231
Jasbeer Singh <jass0013@hotmail.com>
2019-03-14 18:57:33 +0530

    hi

--- iOS Dev/TableView3/TableView3/TableViewController.swift
+++ iOS Dev/TableView3/TableView3/TableViewController.swift
@@ -10,16 +10,64 @@ import UIKit
 
 class TableViewController: UITableViewController,listUpdate {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-         self.navigationItem.rightBarButtonItem = self.editButtonItem
+    var trueFalse:Bool = false
+    
+    @IBOutlet weak var butColor: UIBarButtonItem!
+    
+    @IBAction func Edit(_ sender: Any) {
+        self.tableView.allowsMultipleSelectionDuringEditing = true
+        isEditing = !isEditing
+        if isEditing == true {
+            trueFalse = true
+            butColor.tintColor = UIColor.blue
+        } else if !isEditing == true {
+            trueFalse = false
+            butColor.tintColor = UIColor.gray
+        }
     }
     
+    
+    @IBAction func DeleteBut(_ sender: Any) {
+        if let selectedRows = tableView.indexPathsForSelectedRows {
+            var items = [String]()
+            for indexPath in selectedRows  {
+                items.append(arrayClass.array[indexPath.row])
+            }
+            for item in items {
+                if let index = arrayClass.array.index(of: item) {
+                    arrayClass.array.remove(at: index)
+                }
+            }
+            tableView.beginUpdates()
+            tableView.deleteRows(at: selectedRows, with: .automatic)
+            tableView.endUpdates()
+        }
+    }
+    
+    
+    @IBAction func AddBut(_ sender: Any) {
+        let alert = UIAlertController(title: "Add Data", message: "Enter detail", preferredStyle: .alert)
+        alert.addTextField(configurationHandler: { (textField) in
+            textField.placeholder = "Enter name"
+        })
+        alert.addAction(UIAlertAction(title: "Add", style: .default, handler: { (action) in
+            //Edit data on array
+            let name = alert.textFields![0] as UITextField
+            arrayClass.array.append(name.text!)
+            self.tableView.reloadData()
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        butColor.tintColor = UIColor.gray
+    }
     
     func oneRecord(type: String) {
         arrayClass.array.append(type)
@@ -31,25 +79,24 @@ class TableViewController: UITableViewController,listUpdate {
         if segue.identifier == "EnterDetalVC" {
             let enterDetailVc = segue.destination as! EnterDetailVC
             enterDetailVc.delegate = self
-            
         }
     }
-
+    
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return arrayClass.array.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
         cell.textLabel!.text = arrayClass.array[indexPath.row]
-
+        
         return cell
     }
     
@@ -83,20 +130,23 @@ class TableViewController: UITableViewController,listUpdate {
         return true
     }
     
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let alert = UIAlertController(title: "Edit Data", message: "Edit corresponding detail", preferredStyle: .alert)
-        alert.addTextField(configurationHandler: { (textField) in 
-            textField.placeholder = "Enter name"
-            textField.text = arrayClass.array[indexPath.row]
-        })
-        alert.addAction(UIAlertAction(title: "Edit", style: .default, handler: { (action) in
-            //Edit data on array
-           let name = alert.textFields![0] as UITextField
-            arrayClass.array[indexPath.row] = name.text!
-            tableView.reloadData()
-        }))
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        self.present(alert, animated: true)
+        if (trueFalse == false) {
+            let alert = UIAlertController(title: "Edit Data", message: "Edit corresponding detail", preferredStyle: .alert)
+            alert.addTextField(configurationHandler: { (textField) in
+                textField.placeholder = "Enter name"
+                textField.text = arrayClass.array[indexPath.row]
+            })
+            alert.addAction(UIAlertAction(title: "Edit", style: .default, handler: { (action) in
+                //Edit data on array
+                let name = alert.textFields![0] as UITextField
+                arrayClass.array[indexPath.row] = name.text!
+                tableView.reloadData()
+            }))
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self.present(alert, animated: true)
+        }
     }
     
     /*
