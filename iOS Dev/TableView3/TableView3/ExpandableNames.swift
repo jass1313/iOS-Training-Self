ef0a03cb9519d915b82dd69bae49ef38611cbcd2
Jasbeer Singh <jass0013@hotmail.com>
2019-03-18 18:24:57 +0530

    ,k,

---
+++ iOS Dev/TableView3/TableView3/ExpandableNames.swift
@@ -0,0 +1,16 @@
+//
+//  ExpandableNames.swift
+//  TableView3
+//
+//  Created by KiwiTech on 18/03/19.
+//  Copyright © 2019 KiwiTech. All rights reserved.
+//
+
+import Foundation
+
+  struct ExpandableNames {
+
+    var isExpanded: Bool
+    let names: [String]
+
+}
